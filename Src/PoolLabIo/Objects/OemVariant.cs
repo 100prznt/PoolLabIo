2d5e6ddbf5753abe583919b9be1ff8bfbcb385b1
Elias Ruemmler <elias.ruemmler@de.bosch.com>
2019-04-10 14:21:03 +0200

    Add xml comment

--- Src/PoolLabIo/Objects/OemVariant.cs
+++ Src/PoolLabIo/Objects/OemVariant.cs
@@ -5,11 +5,29 @@
     /// </summary>
     public enum OemVariant : byte
     {
+        /// <summary>
+        /// Internal Version. No LCD output, no measurement types
+        /// </summary>
         Internal,
+        /// <summary>
+        /// Original product. Measurement types 1- 13 and 23 - 24 are available
+        /// </summary>
         PoolLab10,
+        /// <summary>
+        /// Same measurement types as OEM ID 1 (PoolLab)
+        /// </summary>
         Multitest9in1,
+        /// <summary>
+        /// Can only do measurement type 14
+        /// </summary>
         IsoLab10,
+        /// <summary>
+        /// Same measurement types as OEM ID 1 (PoolLab)
+        /// </summary>
         Aquaviva,
+        /// <summary>
+        /// Not yet released. Measurement types 15 - 24 are available
+        /// </summary>
         FineWellPro
     }
 }
