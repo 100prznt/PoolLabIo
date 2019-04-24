baf12eb5739521f2842eb977ce99dbe26d0b01c9
Elias Ruemmler <elias.ruemmler@de.bosch.com>
2019-04-24 14:33:43 +0200

    Update OemVariant.cs

--- Src/PoolLabIo_DLL/Objects/OemVariant.cs
+++ Src/PoolLabIo_DLL/Objects/OemVariant.cs
@@ -9,26 +9,54 @@
         /// <summary>
         /// Internal Version. No LCD output, no measurement types
         /// </summary>
-        Internal,
+        Internal = 0x00,
         /// <summary>
         /// Original product. Measurement types 1- 13 and 23 - 24 are available
         /// </summary>
-        PoolLab10,
+        PoolLab10 = 0x01,
         /// <summary>
         /// Same measurement types as OEM ID 1 (PoolLab)
         /// </summary>
-        Multitest9in1,
+        Multitest9in1 = 0x02,
         /// <summary>
         /// Can only do measurement type 14
         /// </summary>
-        IsoLab10,
+        IsoLab10 = 0x03,
         /// <summary>
         /// Same measurement types as OEM ID 1 (PoolLab)
         /// </summary>
-        Aquaviva,
+        Aquaviva = 0x04,
         /// <summary>
         /// Not yet released. Measurement types 15 - 24 are available
         /// </summary>
-        FineWellPro
+        FineWellPro = 0x05,
+        /// <summary>
+        /// Unknown, OEM ID provided by Water-i.d. GmbH
+        /// </summary>
+        Innovative = 0x06,
+        /// <summary>
+        /// Unknown, OEM ID provided by Water-i.d. GmbH
+        /// </summary>
+        FalkPool = 0x07,
+        /// <summary>
+        /// Unknown, OEM ID provided by Water-i.d. GmbH
+        /// </summary>
+        WaterLab = 0x08,
+        /// <summary>
+        /// Unknown, OEM ID provided by Water-i.d. GmbH
+        /// </summary>
+        CetEnviro = 0x09,
+        /// <summary>
+        /// Unknown, OEM ID provided by Water-i.d. GmbH
+        /// </summary>
+        Trace2O = 0x0A,
+        /// <summary>
+        /// Same measurement types as OEM ID 1 (PoolLab)
+        /// </summary>
+        PoolSana = 0x0B,
+        /// <summary>
+        /// Unknown, OEM ID provided by Water-i.d. GmbH
+        /// </summary>
+        Dutrion = 0x0C
     }
 }
